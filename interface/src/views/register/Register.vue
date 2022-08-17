7808ebdf2e49e9d84d44ddb9548621e64574fba6
Laura Lima <laura_limadias@hotmail.com>
2022-08-17 17:40:27 -0300

    Update Register.vue

--- interface/src/views/register/Register.vue
+++ interface/src/views/register/Register.vue
@@ -1,1 +1,26 @@
-//
+<template>
+  <div class="register">
+    <login />
+  </div>
+</template>
+
+<script>
+
+import Register from '@/components/auth-components/register/RegisterComponent.vue';
+
+export default {
+  components: {
+    Register,
+  },
+};
+{
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
+}
+
+</script>
