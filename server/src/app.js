7722cacb33969bbed869ae28efb2a1029b2d035e
Laura Lima <laura_limadias@hotmail.com>
2022-08-17 16:48:42 -0300

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-//
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const mongooseConnection = require("./config/mongooseConnection.config");
+
+const app = express();
+
+const index = require("./route/index");
+const userRoutes = require("./route/user.routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(morgan("dev"));
+app.use(cors());
+
+app.set("mongoose connection", mongooseConnection);
+
+app.use(index);
+app.use("/api/v1/", userRoutes);
+
+module.exports = app;
