61ad1023e8687ea191550bac9a32236bb80faf1d
Laura Lima <laura_limadias@hotmail.com>
2022-08-17 17:44:30 -0300

    Update index.js

--- interface/src/router/index.js
+++ interface/src/router/index.js
@@ -1,1 +1,60 @@
-//
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NProgress from 'nprogress';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: () => import('../components/auth-components/login/LoginComponent'),
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../components/auth-components/home/HomeComponent'),
+    meta: {
+      requireAuth: true,
+    },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../components/auth-components/register/RegisterComponent'),
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeResolve((to, from, next) => {
+
+  if (to.name) {
+    NProgress.start();
+  }
+  next();
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requireAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/',
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+router.afterEach((to, from) => {
+  NProgress.done();
+});
+
+export default router;
